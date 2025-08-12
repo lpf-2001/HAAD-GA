b83f2df159a4e27e19b7a32364e6fc231f342ad6
lpf-2001 <lpf20011129@gmail.com>
2025-08-12 15:14:55 +0800

    change_test

--- test.py
+++ test.py
@@ -3,12 +3,13 @@ import torch
 from transformers import AutoModelForCausalLM,AutoTokenizer
 model_path = "/root/shared-storage/Qwen-7B"  # 模型所在目录
 tokenizer = AutoTokenizer.from_pretrained(model_path,trust_remote_code=True)
-model = AutoModelForCausalLM.from_pretrained(model_path, trust_remote_code=True, torch_dtype="auto")
+model = AutoModelForCausalLM.from_pretrained(model_path, trust_remote_code=True, torch_dtype="auto").to("cuda")
 count = 0
 with open('reasoning_3sample.txt', 'r', encoding='utf-8') as f:
     for line in f:
         content = line.strip()
         # print(content)
+        
         inputs = tokenizer(content, return_tensors="pt").to(model.device)
         input_ids = inputs["input_ids"]
         with torch.no_grad():
@@ -16,4 +17,6 @@ with open('reasoning_3sample.txt', 'r', encoding='utf-8') as f:
             loss = outputs.loss
             perplexity = torch.exp(loss)
         count = count + 1
+        with open("output.txt", "a") as f:  # "a" 表示追加写入
+            f.write(f"Sentence {count}, Perplexity: {perplexity.item()}\n")
         print(f"Sentence {count}, Perplexity: {perplexity.item()}")
